2ddb0f254f24016dadd6cb4464d8afb7e917303d
MattL28 <alleynematthaus@gmail.com>
2024-06-18 18:23:59 -0400

    added walking orange gif and changes being made to withdraw widget tsx

--- src/components/virtualizer/WithdrawWidget.tsx
+++ src/components/virtualizer/WithdrawWidget.tsx
@@ -11,6 +11,17 @@ import { parseEther } from "viem";
 import { MUSD_ADDRESS, VIRTUALIZER_ADDRESS } from "@/constants/addresses";
 import { toast } from "sonner";
 
+interface CustomToastProps {
+  message: string;
+  gifUrl: string;
+}
+
+const CustomToast: React.FC<CustomToastProps> = ({ message, gifUrl }) => (
+  <div className="flex items-center">
+    <img src={gifUrl} alt="Toast Icon" />
+    <span>{message}</span>
+  </div>
+);
 interface WithdrawWidgetProps {
   refreshBalance: () => void; // Define the type of refreshBalance as a function
   balance: number;
@@ -63,7 +74,17 @@ const WithdrawWidget: React.FC<WithdrawWidgetProps> = ({ refreshBalance, balance
 
   useEffect(() => {
     if (isConfirming) {
-      toast.loading("Transaction Pending");
+      toast.loading(<CustomToast message="TransactionPending" gifUrl="walking_orange.gif"/>,{
+        /*style:{
+          background:"#3A4047",
+          width:"459px",
+          height:"576px",
+          top:"145px",
+          left:"490px",
+          position:"fixed"
+        },*/
+        className:"bg-[#3A4047] w-full h-full"
+      });
     }
     toast.dismiss();
       
