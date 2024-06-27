b306ae7f76c6eea84007afee61436eb7afda9225
tonyolendo <teeolendo@gmail.com>
2024-06-27 19:08:47 -0400

    ui enhancements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,30 +75,30 @@ export default function Home() {
           </>
         ) : (
           <>
-            <section className="py-12 flex flex-col items-center text-center gap-8">
+            <section className="py-12 flex flex-col">
               <div className="flex gap-6 items-center justify-center">
-                <div className="font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+                <div className="font-semibold text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
                     {tabs.map((tab) => (
                         <button
                           key={tab.id}
                           onClick={() => setActiveTab(tab.id)}
-                          className={`flex-1 py-2 px-4 text-center focus:outline-none ${
+                          className={`flex-1 py-2 px-4 mx-12 text-center focus:outline-none ${
                             activeTab === tab.id
-                              ? 'inline-block p-6 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500'
-                              : 'inline-block p-6 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300'
+                              ? 'text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500'
+                              : 'border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300'
                           }`}
                         >
-                          <>
-                          <Image
-                            src={tab.logo}
-                            alt="virtualizer-logo"
-                            width={24}
-                            height={24}
-                            priority
-                          />
-                          
-                          {tab.label}
-                          </>
+                          <div className="flex flex-row gap-4 items-center">
+                            <Image
+                              src={tab.logo}
+                              alt="virtualizer-logo"
+                              width={32}
+                              height={32}
+                              priority
+
+                            />
+                            {tab.label}
+                          </div>
                         </button>
                     ))}
                   </div>
